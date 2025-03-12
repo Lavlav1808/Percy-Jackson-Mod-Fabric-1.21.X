7382e92cbf695df547067368eefda38dd6d67c04
Lavlav1808 <lavi180814@gmail.com>
2025-03-12 17:38:36 +0200

    Initial commit

---
+++ src/main/java/net/lavlav/pjmod/PercyJacksonMod.java
@@ -0,0 +1,16 @@
+package net.lavlav.pjmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PercyJacksonMod implements ModInitializer {
+	public static final String MOD_ID = "pjmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lavlav/pjmod/PjModClient.java
@@ -0,0 +1,10 @@
+package net.lavlav.pjmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PjModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
